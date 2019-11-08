02fa51d7dd35d9fd23dea725f24008ba64a846ab
“Kgotso-Koete” <kgkoete@gmail.com>
2019-11-08 21:42:17 +0200

    Enabling Commenting functionality on Articles

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ if (isProduction) {
 
 require("./models/User");
 require("./models/Article");
+require("./models/Comment");
 require("./config/passport");
 
 app.use(require("./routes"));
--- routes/api/articles.js
+++ routes/api/articles.js
@@ -3,6 +3,7 @@ var passport = require("passport");
 var mongoose = require("mongoose");
 var Article = mongoose.model("Article");
 var User = mongoose.model("User");
+var Comment = mongoose.model("Comment");
 var auth = require("../auth");
 
 // Make the endpoint for creating articles for logged in users
@@ -138,6 +139,101 @@ router.delete("/:article/favorite", auth.required, function(req, res, next) {
       });
     })
     .catch(next);
+});
+
+// Create an endpoint to create comments on articles
+// POST /api/articles/:slug/comments
+router.post("/:article/comments", auth.required, function(req, res, next) {
+  User.findById(req.payload.id)
+    .then(function(user) {
+      if (!user) {
+        return res.sendStatus(401);
+      }
+
+      var comment = new Comment(req.body.comment);
+      comment.article = req.article;
+      comment.author = user;
+
+      return comment.save().then(function() {
+        req.article.comments = req.article.comments.concat([comment]);
+
+        return req.article.save().then(function(article) {
+          res.json({ comment: comment.toJSONFor(user) });
+        });
+      });
+    })
+    .catch(next);
+});
+
+// Create an endpoint to list comments on articles
+// GET /api/articles/:slug/comments
+router.get("/:article/comments", auth.optional, function(req, res, next) {
+  Promise.resolve(req.payload ? User.findById(req.payload.id) : null)
+    .then(function(user) {
+      return req.article
+        .populate({
+          path: "comments",
+          populate: {
+            path: "author"
+          },
+          options: {
+            sort: {
+              createdAt: "desc"
+            }
+          }
+        })
+        .execPopulate()
+        .then(function(article) {
+          return res.json({
+            comments: req.article.comments.map(function(comment) {
+              return comment.toJSONFor(user);
+            })
+          });
+        });
+    })
+    .catch(next);
+});
+
+// Router param middleware for resolving get comment details before DELETE is called
+router.param("comment", function(req, res, next, id) {
+  Comment.findById(id)
+    .then(function(comment) {
+      if (!comment) {
+        return res.sendStatus(404);
+      }
+
+      req.comment = comment;
+
+      return next();
+    })
+    .catch(next);
+});
+
+// Create an endpoint for comment authors to delete comments on articles
+// DELETE /api/articles/:slug/comments/:id
+router.delete("/:article/comments/:comment", auth.required, function(
+  req,
+  res,
+  next
+) {
+  // Check that the currently logged in user is the comment author
+  if (req.comment.author.toString() === req.payload.id.toString()) {
+    // Remove the comment ID from the article model
+    req.article.comments.remove(req.comment._id);
+    req.article
+      .save()
+      .then(
+        // delete the actual comment from the database
+        Comment.find({ _id: req.comment._id })
+          .remove()
+          .exec()
+      )
+      .then(function() {
+        res.sendStatus(204);
+      });
+  } else {
+    res.sendStatus(403);
+  }
 });
 
 module.exports = router;
